cf645e4f939c6206a69bc54a1898efa3fc15eb34
shinmike <shinmike90@gmail.com>
2017-06-28 01:46:41 +0000

    fixed the layout of username and content

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,13 +4,9 @@ class Message extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="message">
         <span className="message-username">{this.props.username}</span>
         <span className="message-content">{this.props.content}</span>
-
-        <div className="message system">
-          {/*Anonymous1 changed their name to nomnom.*/}
-        </div>
       </div>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -5,17 +5,18 @@ class MessageList extends React.Component {
 
   render() {
     return (
-      <div>
+      <main className="messages" >
         {this.props.messages.map((message) => {
           return ( // return messages array (in App.jsx) and loop
-            <div className="message" key={message.id}>
-              <main className="messages">
-                <Message username={message.username} content={message.content} />
-              </main>
+            <div key={message.id}>
+              <Message username={message.username} content={message.content} />
             </div>
           );
         })}
-      </div>
+        {/*<div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>  */}
+      </main>
     );
   }
 
