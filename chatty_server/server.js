e343815ec66c56d825490b5b95099b29add0b81b
shinmike <shinmike90@gmail.com>
2017-06-29 01:43:49 +0000

    Displaying Incoming Messages

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,30 +1,32 @@
 const express = require('express');
 const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const uuidv1 = require('uuid/v1');
 
-// Set the port to 3001
 const PORT = 3001;
 
-// Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
 const wss = new SocketServer({ server });
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  ws.on('message', (message) => {
-    const id = JSON.parse(message).id;
-    const username = JSON.parse(message).username;
-    const content = JSON.parse(message).content;
-    console.log(`User ${username} with id: ${id} sent "${content}"`);
-  });
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('message', (message) => {
+    const newMessage = JSON.parse(message);
+    newMessage.id = uuidv1();
+    wss.broadcast(JSON.stringify(newMessage));
+  });
+  
   ws.on('close', () => console.log('Client disconnected'));
 });
--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,16 @@ class App extends React.Component {
     super(props); // allow children to access props from this parent state
     this.state = { // parent's state listed
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          id: 1,
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          id: 2,
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      messages: [] // messages coming from the server will be stored here as they arrive
     };
 
     this.socket = new WebSocket("ws://localhost:3001");
+    
+    this.socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      const newState = this.state.messages.concat(message); // concatenates new message to exisiting messages
+      this.setState({ messages: newState }); //sets updated messages
+    };
 
     this.sendMessageToServer = this.sendMessageToServer.bind(this);
     
@@ -38,13 +33,12 @@ class App extends React.Component {
   sendMessageToServer(newMessage) {
     console.log('Message to server is', newMessage);
     const message = {
-      id: Math.random(),
       username: this.state.currentUser.name,
       content: newMessage
-    } // creating the new user's id, username, content
+    } 
     this.socket.send(JSON.stringify(message)); // send to server
-    const allMessages = this.state.messages.concat(message); // concatenates new message to exisiting messages
-    this.setState({messages: allMessages}); //sets updated messages
+    // const allMessages = this.state.messages.concat(message); 
+    // this.setState({messages: allMessages}); 
   }		   
 
   render() {
